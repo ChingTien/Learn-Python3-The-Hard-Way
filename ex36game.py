8bec7fd73ae6eb79a17cb93a69312abc16403002
_________C_________ <39040963+ChingTien@users.noreply.github.com>
2019-01-06 16:02:39 +0800

    Update ex36game.py

--- ex36game.py
+++ ex36game.py
@@ -3,14 +3,14 @@ from sys import exit
 #feed the baby
 def baby_food():
     print("what will you feed your baby?")
-    food = input("Milk or Cookie?  ")
+    food = input("Milk or Cookie?")
 
-    if "milk" or "Milk" in food:
+    if "milk" in food:
         print("the baby is happy, you saved the day!")
         print("嬰兒今天喝仙草牛奶，超可愛")
         print("你贏了")
         exit(0)
-    elif "cookie" or "Cookie" in food:
+    elif "cookie" in food:
         print("嬰兒不能吃餅乾！餅乾是娃子的啦！！！")
         print("再見了")
         exit(0)
@@ -25,13 +25,13 @@ def start():
 
     choice = input(">")
     
-    if "feed" or "Feed" in choice:
+    if "feed" in choice:
         baby_food()
-    elif "comfort" or "Comfort" in choice:
+    elif "comfort" in choice:
         print("嬰兒有人安慰，很高興")
         print("你贏了！")
         exit(0)
-    elif "ignore" or "Ignore"in choice:
+    elif "ignore" in choice:
         print("嬰兒酸掉了")
         print("再見了")
         exit(0)
